0ab5a1de17e9755f0a41c3801821ec3254f43895
Your Name <getitnowmarketing@gmail.com>
2011-09-17 13:50:22 -0400

    disable debugging and fix sched.c error

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 53 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#53 PREEMPT Thu Sep 1 21:54:53 EDT 2011"
-#define LINUX_COMPILE_TIME "21:54:53"
-#define LINUX_COMPILE_BY "drew"
-#define LINUX_COMPILE_HOST "tinster"
-#define LINUX_COMPILE_DOMAIN "localdomain"
-#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
